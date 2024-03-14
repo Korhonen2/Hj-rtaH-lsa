696ff038a15c2e077e6f06bf838d523c2552b4ee
Korhonen2 <jessicamkorhonen@hotmail.com>
2024-03-14 09:43:27 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function buildMenu()
+{
+    var links = [
+        ["Startsida", "index.html"],
+        ["Behandlingar", "behandlingar.html"],
+        ["Webbshop", "Webbshop.html"],
+        ["HLR", "hlr.html"],
+        ["Om oss", "omoss.html"],
+        ["Kontakt", "kontakt.html"],
+    ]
+        var content = ""
+    for(var i = 0; i < links.length; i++)
+    {
+        var text = links[i][0]
+        var adress = links[i][1]
+        content = content + "<li><a href=\"" + adress + "\">" + text + "</a></li>"
+        // <li><a href="index.html">Startsidan</a></li>
+    }
+
+
+    document.getElementById("menu").innerHTML = content
+}
